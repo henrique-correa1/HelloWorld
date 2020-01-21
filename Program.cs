50b7cf07e2511a084d325af0b24e8ad74f598410
Henrique Faria Brito Martini Correa <hcorrea@usf.edu>
2020-01-21 17:33:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to diplay Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
